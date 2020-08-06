b076c21763837a7c423ea8e3f5823a0689e1e722
Stephen Paul Weber <singpolyma@singpolyma.net>
2020-08-05 20:56:11 -0500

    Initial commit
    This code is garbage and will have to be rewritten.
    
    When paired with the Asterisk setup, inbound calls to sip:some%40jid@server.tld
    end up as working Jingle calls (tested only with Conversations) to the target
    JID.
    
    Declining the call, hanging up, and errors from calling bad JIDs all are handled.

---
+++ Util.hs
@@ -0,0 +1,183 @@
+module Util where
+
+import Prelude ()
+import BasicPrelude
+import Control.Concurrent.STM          (STM, atomically)
+import Control.Applicative             (many)
+import Control.Concurrent
+	(ThreadId, forkFinally, myThreadId, throwTo)
+import Data.Void                       (absurd)
+import Control.Error                   (exceptT)
+import Data.Time.Clock                 (UTCTime)
+import Data.Time.Format                (parseTimeM, defaultTimeLocale)
+import qualified Control.Exception     as Ex
+import qualified Data.Attoparsec.Text  as Atto
+import qualified Data.Text             as Text
+import qualified Data.XML.Types        as XML
+import qualified Network.Protocol.XMPP as XMPP
+import           UnexceptionalIO.Trans (Unexceptional (lift))
+import qualified UnexceptionalIO.Trans as UIO
+
+instance Unexceptional XMPP.XMPP where
+	lift = liftIO . UIO.run
+
+s :: (IsString s) => String -> s
+s = fromString
+
+fromIO_ :: (Unexceptional m) => IO a -> m a
+fromIO_ = exceptT absurd return . UIO.fromIO' (error . show)
+
+atomicUIO :: (Unexceptional m) => STM a -> m a
+atomicUIO = fromIO_ . atomically
+
+escapeJid :: Text -> Text
+escapeJid txt = mconcat result
+	where
+	Right result = Atto.parseOnly (many (
+			slashEscape <|>
+			replace ' ' "\\20" <|>
+			replace '"' "\\22" <|>
+			replace '&' "\\26" <|>
+			replace '\'' "\\27" <|>
+			replace '/' "\\2f" <|>
+			replace ':' "\\3a" <|>
+			replace '<' "\\3c" <|>
+			replace '>' "\\3e" <|>
+			replace '@' "\\40" <|>
+			fmap Text.singleton Atto.anyChar
+		) <* Atto.endOfInput) txt
+	replace c str = Atto.char c *> pure (fromString str)
+
+-- XEP-0106 says to only escape \ when absolutely necessary
+slashEscape :: Atto.Parser Text
+slashEscape =
+	fmap (s"\\5c"++) $
+	Atto.char '\\' *> Atto.choice escapes
+	where
+	escapes = map (Atto.string . fromString) [
+			"20", "22", "26", "27", "2f", "3a", "3c", "3e", "40",
+			"5c"
+		]
+
+unescapeJid :: Text -> Text
+unescapeJid txt = fromString result
+	where
+	Right result = Atto.parseOnly (many (
+			(Atto.char '\\' *> Atto.choice unescapes) <|>
+			Atto.anyChar
+		) <* Atto.endOfInput) txt
+	unescapes = map (\(str, c) -> Atto.string (fromString str) *> pure c) [
+			("20", ' '), ("22", '"'), ("26", '&'), ("27", '\''),
+			("2f", '/'), ("3a", ':'), ("3c", '<'), ("3e", '>'),
+			("40", '@'), ("5c", '\\')
+		]
+
+castException :: (Ex.Exception e1, Ex.Exception e2) => e1 -> Maybe e2
+castException = Ex.fromException . Ex.toException
+
+-- Re-throws all by ThreadKilled async to parent thread
+-- Makes sync child exceptions async in parent, which is a bit sloppy
+forkXMPP :: XMPP.XMPP () -> XMPP.XMPP ThreadId
+forkXMPP kid = do
+	parent <- liftIO myThreadId
+	session <- XMPP.getSession
+	liftIO $ forkFinally
+		(void $ XMPP.runXMPP session kid)
+		(either (handler parent) (const $ return ()))
+	where
+	handler parent e
+		| Just Ex.ThreadKilled <- castException e = return ()
+		| otherwise = throwTo parent e
+
+iqReply :: Maybe XML.Element -> XMPP.IQ -> XMPP.IQ
+iqReply payload iq = iq {
+	XMPP.iqType = XMPP.IQResult,
+	XMPP.iqFrom = XMPP.iqTo iq,
+	XMPP.iqTo = XMPP.iqFrom iq,
+	XMPP.iqPayload = payload
+}
+
+iqError :: XML.Element -> XMPP.IQ -> XMPP.IQ
+iqError payload iq = (iqReply (Just payload) iq) {
+	XMPP.iqType = XMPP.IQError
+}
+
+messageError :: XML.Element -> XMPP.Message -> XMPP.Message
+messageError payload message = message {
+	XMPP.messageType = XMPP.MessageError,
+	XMPP.messageFrom = XMPP.messageTo message,
+	XMPP.messageTo = XMPP.messageFrom message,
+	XMPP.messagePayloads = payload : XMPP.messagePayloads message
+}
+
+notImplemented :: XML.Element
+notImplemented =
+	errorPayload "cancel" "feature-not-implemented" (s"Unknown request") []
+
+child :: (XMPP.Stanza s) => XML.Name -> s -> Maybe XML.Element
+child name = listToMaybe .
+	(XML.isNamed name <=< XMPP.stanzaPayloads)
+
+errorChild :: (XMPP.Stanza s) => s -> Maybe XML.Element
+errorChild = child (s"{jabber:component:accept}error")
+
+getBody :: (XMPP.Stanza s) => s -> Maybe Text
+getBody = fmap (mconcat . XML.elementText) .
+	child (s"{jabber:component:accept}body")
+
+getSubject :: (XMPP.Stanza s) => s -> Maybe Text
+getSubject = fmap (mconcat . XML.elementText) .
+	child (s"{jabber:component:accept}subject")
+
+errorPayload :: String -> String -> Text -> [XML.Node] -> XML.Element
+errorPayload typ definedCondition english morePayload =
+	XML.Element (s"{jabber:component:accept}error")
+	[(s"type", [XML.ContentText $ fromString typ])]
+	(
+		(
+			XML.NodeElement $ XML.Element definedConditionName [] []
+		) :
+		(
+			XML.NodeElement $ XML.Element
+				(s"{urn:ietf:params:xml:ns:xmpp-stanzas}text")
+				[(s"xml:lang", [XML.ContentText $ s"en"])]
+				[XML.NodeContent $ XML.ContentText english]
+		) :
+		morePayload
+	)
+	where
+	definedConditionName = fromString $
+		"{urn:ietf:params:xml:ns:xmpp-stanzas}" ++ definedCondition
+
+bareJid :: XMPP.JID -> XMPP.JID
+bareJid (XMPP.JID node domain _) = XMPP.JID node domain Nothing
+
+bareTxt :: XMPP.JID -> Text
+bareTxt (XMPP.JID (Just node) domain _) =
+	mconcat [XMPP.strNode node, s"@", XMPP.strDomain domain]
+bareTxt (XMPP.JID Nothing domain _) = XMPP.strDomain domain
+
+parseXMPPTime :: Text -> Maybe UTCTime
+parseXMPPTime =
+	parseTimeM False defaultTimeLocale "%Y-%m-%dT%H:%M:%S%QZ" . textToString
+
+mkElement :: XML.Name -> Text -> XML.Element
+mkElement name content = XML.Element name []
+	[XML.NodeContent $ XML.ContentText content]
+
+mkDiscoIdentity :: Text -> Text -> Text -> XML.Element
+mkDiscoIdentity category typ name =
+	XML.Element (s"{http://jabber.org/protocol/disco#info}identity") [
+		(s"category", [XML.ContentText category]),
+		(s"type", [XML.ContentText typ]),
+		(s"name", [XML.ContentText name])
+	] []
+
+mkDiscoFeature :: Text -> XML.Element
+mkDiscoFeature var =
+	XML.Element (s"{http://jabber.org/protocol/disco#info}feature") [
+		(s"var", [XML.ContentText var])
+	] []
+
+(.:) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
+(.:) = (.) . (.)
---
+++ gateway.hs
@@ -0,0 +1,184 @@
+module Main (main) where
+
+import Prelude ()
+import BasicPrelude
+import System.IO
+	(stdout, stderr, hSetBuffering, BufferMode(LineBuffering))
+import Control.Concurrent              (threadDelay)
+import Control.Concurrent.STM          (STM)
+import Control.Error                   (exceptT, ExceptT(..), headZ, throwE)
+import Control.Lens                    (over, set, at, _Right, traverseOf)
+import Network                         (PortID (PortNumber))
+import System.Clock                    (TimeSpec(..))
+import Data.Time.Clock                 (getCurrentTime)
+import qualified Focus
+import qualified Data.Text as T
+import qualified Data.Cache as Cache
+import qualified Network.Protocol.XMPP as XMPP
+import qualified Network.Protocol.XMPP.Internal as XMPP
+import qualified Data.XML.Types as XML
+
+import Util
+
+Just asteriskJid = XMPP.parseJID $ s"asterisk"
+
+rewriteJingleInitiatorResponder iq
+	| Just jingle <- child (s"{urn:xmpp:jingle:1}jingle") iq = iq {
+			XMPP.iqPayload = Just $ jingle {
+				XML.elementAttributes = map initiatorResponder (XML.elementAttributes jingle)
+			}
+		}
+	| otherwise = iq
+	where
+	initiatorResponder (name, content)
+		| name == s"initiator" = (name, [XML.ContentText $ maybe (s"") XMPP.formatJID (XMPP.iqFrom iq)])
+		| name == s"responder" = (name, [XML.ContentText $ maybe (s"") XMPP.formatJID (XMPP.iqFrom iq)])
+		| otherwise = (name, content)
+
+bounceStanza (XMPP.ReceivedMessage m) from to =
+	XMPP.putStanza $ m {
+		XMPP.messageFrom = Just from,
+		XMPP.messageTo = Just to
+	}
+bounceStanza (XMPP.ReceivedIQ iq) from to =
+	XMPP.putStanza $ rewriteJingleInitiatorResponder $ iq {
+		XMPP.iqFrom = Just from,
+		XMPP.iqTo = Just to
+	}
+bounceStanza (XMPP.ReceivedPresence p) from to =
+	XMPP.putStanza $ p {
+		XMPP.presenceFrom = Just from,
+		XMPP.presenceTo = Just to
+	}
+
+asteriskToReal :: XMPP.JID -> Maybe XMPP.JID -> Maybe (XMPP.JID, XMPP.JID)
+asteriskToReal componentJid (Just XMPP.JID {
+	XMPP.jidNode = Just escapedTo,
+	XMPP.jidResource = Just escapedFrom
+}) = (,) <$> XMPP.parseJID (unescapeJid $ XMPP.strNode escapedTo) <*>
+	XMPP.parseJID (
+		(escapeJid $ unescapeJid $ XMPP.strResource escapedFrom) ++ s"@" ++
+		bareTxt componentJid ++ s"/sip"
+	)
+asteriskToReal _ _ = Nothing
+
+realToAsterisk :: XMPP.JID -> Maybe XMPP.JID -> Maybe XMPP.JID -> Maybe XMPP.JID
+realToAsterisk componentJid (Just from) (Just XMPP.JID {
+	XMPP.jidNode = Just escapedTo
+}) = XMPP.parseJID $
+	(escapeJid $ bareTxt from) ++ s"@" ++
+	bareTxt componentJid ++ s"/" ++
+	(escapeJid $ unescapeJid $ XMPP.strNode escapedTo)
+realToAsterisk _ _ _ = Nothing
+
+receivedFrom (XMPP.ReceivedMessage s) = XMPP.stanzaFrom s
+receivedFrom (XMPP.ReceivedPresence s) = XMPP.stanzaFrom s
+receivedFrom (XMPP.ReceivedIQ s) = XMPP.stanzaFrom s
+
+receivedTo (XMPP.ReceivedMessage s) = XMPP.stanzaTo s
+receivedTo (XMPP.ReceivedPresence s) = XMPP.stanzaTo s
+receivedTo (XMPP.ReceivedIQ s) = XMPP.stanzaTo s
+
+jingleSid (XMPP.ReceivedIQ iq)
+	| Just jingle <- child (s"{urn:xmpp:jingle:1}jingle") iq =
+		XML.attributeText (s"sid") jingle
+jingleSid _ = Nothing
+
+sessionInitiateId (XMPP.ReceivedIQ iq)
+	| Just jingle <- child (s"{urn:xmpp:jingle:1}jingle") iq,
+	  XML.attributeText (s"action") jingle == Just (s"session-initiate") =
+		((,) iq) <$> XML.attributeText (s"sid") jingle
+sessionInitiateId _ = Nothing
+
+main :: IO ()
+main = do
+	hSetBuffering stdout LineBuffering
+	hSetBuffering stderr LineBuffering
+
+	(componentJidTxt:host:portTxt:secret:[]) <- getArgs
+	let Just componentJid = XMPP.parseJID componentJidTxt
+	let port = PortNumber $ read portTxt
+	let server = XMPP.Server componentJid (textToString host) port
+
+	sessionInitiates <- Cache.newCache (Just $ TimeSpec 900 0)
+	fullJids <- Cache.newCache (Just $ TimeSpec 900 0)
+	-- exceptT print return $ runRoutedComponent server secret $ do
+
+	Right () <- XMPP.runComponent server secret $ forever $ do
+		stanza <- XMPP.getStanza
+		case receivedFrom stanza of
+			_ | XMPP.ReceivedIQ iq <- stanza,
+			    XMPP.iqID iq == Just (s"CHEOGRAMIGNORE") -> return ()
+			Just sfrom
+				| sfrom == asteriskJid,
+				  Just (iq, sid) <- sessionInitiateId stanza -> do
+					let Just (to, from) = asteriskToReal componentJid $ receivedTo stanza
+					liftIO $ Cache.purgeExpired sessionInitiates
+					liftIO $ Cache.insert sessionInitiates sid iq
+					XMPP.putStanza $ (XMPP.emptyMessage XMPP.MessageNormal) {
+							XMPP.messageID = Just $ s"proposal%" ++ sid,
+							XMPP.messageTo = Just to,
+							XMPP.messageFrom = Just from,
+							XMPP.messagePayloads = [
+								XML.Element (s"{urn:xmpp:jingle-message:0}propose")
+									[(s"id", [XML.ContentText sid])]
+									[XML.NodeElement $ XML.Element (s"{urn:xmpp:jingle:apps:rtp:1}description") [(s"media", [XML.ContentText $ s"audio"])] []]
+							]
+						}
+			Just sfrom | sfrom == asteriskJid ->
+				let
+					Just (to, from) = asteriskToReal componentJid $ receivedTo stanza
+					msid = jingleSid stanza
+				in do
+					fullTo <- liftIO $ maybe (return Nothing) (Cache.lookup' fullJids) msid
+					liftIO $ forM_ msid $ \sid -> forM_ fullTo $ Cache.insert fullJids sid
+					bounceStanza stanza from (fromMaybe to fullTo)
+			sfrom
+				| XMPP.ReceivedMessage m <- stanza,
+				  Just from <- sfrom,
+				  Just to <- XMPP.stanzaTo m,
+				  Just proceed <- child (s"{urn:xmpp:jingle-message:0}proceed") m -> do
+					let sid = fromMaybe mempty $ XML.attributeText (s"id") proceed
+					minit <- liftIO $ Cache.lookup' sessionInitiates sid
+					forM_ minit $ \init -> do
+						liftIO $ Cache.delete sessionInitiates sid
+						liftIO $ Cache.insert fullJids sid from
+						bounceStanza (XMPP.ReceivedIQ init) to from
+				| XMPP.ReceivedMessage m <- stanza,
+				  Just from <- sfrom,
+				  Just to <- XMPP.stanzaTo m,
+				  Just reject <- child (s"{urn:xmpp:jingle-message:0}reject") m -> do
+					let sid = fromMaybe mempty $ XML.attributeText (s"id") reject
+					minit <- liftIO $ Cache.lookup' sessionInitiates sid
+					forM_ minit $ \init -> do
+						liftIO $ Cache.delete sessionInitiates sid
+						XMPP.putStanza $ iqReply Nothing init
+						XMPP.putStanza $ (XMPP.emptyIQ XMPP.IQSet) {
+								XMPP.iqID = Just $ s"CHEOGRAMIGNORE",
+								XMPP.iqTo = XMPP.iqFrom init,
+								XMPP.iqFrom = XMPP.iqTo init,
+								XMPP.iqPayload = Just $ XML.Element
+									(s"{urn:xmpp:jingle:1}jingle")
+									[
+										(s"action", [XML.ContentText $ s"session-terminate"]),
+										(s"sid", [XML.ContentText $ sid])
+									]
+									[XML.NodeElement $ XML.Element (s"{urn:xmpp:jingle:1}reason") [] [
+										XML.NodeElement $ XML.Element (s"{urn:xmpp:jingle:1}decline") [] []
+									]]
+							}
+				| XMPP.ReceivedMessage m <- stanza,
+				  XMPP.messageType m == XMPP.MessageError,
+				  (errPayload:_) <- XMPP.messagePayloads m,
+				  Just sid <- T.stripPrefix (s"proposal%") =<< XMPP.messageID m -> do
+					minit <- liftIO $ Cache.lookup' sessionInitiates sid
+					forM_ minit $ \init -> do
+						liftIO $ Cache.delete sessionInitiates sid
+						XMPP.putStanza $ iqError errPayload init
+				| otherwise ->
+					let
+						Just from = realToAsterisk componentJid sfrom (receivedTo stanza)
+					in
+					bounceStanza stanza from asteriskJid
+
+	return ()
